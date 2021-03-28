2974e5324bb217f9104c3d91621d4ce7d31bbb8c
최창준 <aowert6518@naver.com>
2021-03-28 18:25:23 +0900

    first commit

---
+++ src/main/java/jpaself/self_WowWe/SelfWowWeApplication.java
@@ -0,0 +1,13 @@
+package jpaself.self_WowWe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfWowWeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfWowWeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jpaself/self_WowWe/SelfWowWeApplicationTests.java
@@ -0,0 +1,13 @@
+package jpaself.self_WowWe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfWowWeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
